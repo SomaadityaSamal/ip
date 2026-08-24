dba45bc4f318e999a3b996537116551f7d4a9c8a
Samal <somaadityasamal@gmail.com>
2026-08-24 12:27:16 +0800

    Add Level-0

---
+++ src/main/java/Friday.java
@@ -0,0 +1,10 @@
+public class Friday {
+    public static void main(String[] args) {
+        String banner = "_____ ____  ___ ____    _ __   __\n" +
+                "|  ___|  _ \\|_ _|  _ \\  / \\\\ \\ / /\n" +
+                "| |_  | |_) || || | | |/ _ \\\\ V / \n" +
+                "|  _| |  _ < | || |_| / ___ \\| |  \n" +
+                "|_|   |_| \\_\\___|____/_/   \\_\\_|";
+        System.out.println(banner);
+    }
+}
